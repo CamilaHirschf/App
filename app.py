f7b448cdef9f02437ede020efc58942de6cc18a9
CamilaHirschf <94084446+CamilaHirschf@users.noreply.github.com>
2023-12-06 13:00:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def index():
+   '''Ruta de la página de índice'''
+   return '<h1>¡Hola Mundo!</h1>'
+
+if __name__ == '__main__':
+   app.run(debug=False)
+
